9b44c9152c27051d05ea4f1a8975af1eebcc858e
Leirannt <93717185+Leirannt@users.noreply.github.com>
2023-03-20 01:31:48 +0700

    menambahkan class DataGeneric

--- modul5_1302210128/Program.cs
+++ modul5_1302210128/Program.cs
@@ -2,19 +2,31 @@
 
 namespace modul5_1302210128
 {
-    internal class Program
+internal class Program
+{
+    public class HaloGeneric
     {
-        public class HaloGeneric
-        {
-            public static void sapaUser<T>(T inputuser)
-            {
-                Console.WriteLine("Hallo user " + inputuser);
-            }
-        }
-
-        private static void Main(string[] args)
-        {
-            HaloGeneric.sapaUser<string>("Ariel");
+         public static void sapaUser<T>(T inputuser){
+            Console.WriteLine("Hallo user " + inputuser);
         }
     }
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        HaloGeneric.sapaUser<string>("Ariel");
+        DataGeneric<string> nim = new DataGeneric<string>("1302210128");
+        nim.PrintData();
+    }
+}
 }
